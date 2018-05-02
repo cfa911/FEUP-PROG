440859394443680e977ec85d75780c3697735344
Christopher Abreu <cfa911@outlook.pt>
2018-05-02 09:29:14 +0100

    3rd Commit

---
+++ Board.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Board
+{
+public:
+	//Board functions and param
+private:
+};
---
+++ Puzzle.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Puzzle
+{
+public:
+	//Puzzle functions and param
+private:
+};
