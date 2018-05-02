d215113ab54fc3cf9780d3c9d4c6cf34b55e76d3
Christopher Abreu <cfa911@outlook.pt>
2018-05-02 09:25:15 +0100

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int    i =3;
+	double d;
+
+	// declare reference variables
+	int&    r = i;
+	double& s = d;
+	cout << r;
+	system("pause");
+}
