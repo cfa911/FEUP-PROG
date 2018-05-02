9c395a2dc51d971fd043fa0a2888e10acc51dbb9
Christopher Abreu <cfa911@outlook.pt>
2018-05-02 09:28:09 +0100

    2nd Commit

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,83 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <algorithm>
+#include <set>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	int    i =3;
-	double d;
 
-	// declare reference variables
-	int&    r = i;
-	double& s = d;
-	cout << r;
-	system("pause");
+void insertwords(string line, set<string> &words) {
+	string a;
+	int i = 1;
+	size_t npos;
+	while (line.size() != 0)
+	{
+		if (i == 1)
+		{
+			npos = line.find_first_of(":");
+			if (npos > line.size())
+			{
+				a = line.substr(0, line.size());
+				line.clear();
+				words.insert(a);
+				a.clear();
+			}
+			else
+			{
+				a = line.substr(0, npos);
+				line.erase(0, npos + 1);
+				words.insert(a);
+				a.clear();
+			}
+			i = i - 1;
+		}
+		else
+		{
+			npos = line.find_first_of(",");
+			if (npos > line.size())
+			{
+				a = line.substr(0, line.size());
+				line.clear();
+				words.insert(a);
+				a.clear();
+			}
+			else
+			{
+				a = line.substr(0, npos);
+				line.erase(0, npos + 1);
+				words.insert(a);
+				a.clear();
+			}
+		}
+	}
+
+}
+
+int main() {
+	set<string> words;
+	string my_file_name, line;
+	ifstream my_file;
+	cout << "File name?" << endl;
+	cin >> my_file_name;
+	my_file.open(my_file_name);
+	if (my_file.fail())
+	{
+		cout << "File could not be open" << endl;
+		return 0;
+	}
+	else
+	{
+		while (!my_file.eof())
+		{
+			getline(my_file, line);
+			insertwords(line, words);
+		}
+	}
+
+
+
+
+	return 0;
 }
